038411639cd37604192dca39aba5f24405e574ec
xianghao <xianghao1991@gmail.com>
2017-01-20 18:30:13 +0800

    clock

--- apps/clock/main.py
+++ apps/clock/main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+from time import strftime
 
 LabelBase.register(
     name='Roboto',
@@ -9,10 +14,15 @@ LabelBase.register(
 
 
 class ClockApp(App):
-    pass
+    def update_time(self, seconds):
+        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
 
 
 def run():
+    Window.clearcolor = get_color_from_hex('#301216')
     ClockApp().run()
 
 
